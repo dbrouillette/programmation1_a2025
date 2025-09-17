d2354e555d2fe01c297e1d9d39e43fdb58b7f8eb
David Brouillette - Enseignant Cégep de Trois-Rivières <63271086+dbrouillette@users.noreply.github.com>
2025-09-17 08:54:59 -0400

    Update Demot15Sept.java

--- Demo/Demot15Sept.java
+++ Demo/Demot15Sept.java
@@ -1,26 +1,41 @@
 package Demo;
 
+import java.nio.channels.Pipe.SourceChannel;
 import java.util.Scanner;
 
 public class Demot15Sept {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Entrez un nombre entier : ");
-        if(sc.hasNextInt() == true){
-            int nombre = sc.nextInt();
-            System.out.println("Vous avez entré : " + nombre);
-
-            // ici 
+        Scanner console = new Scanner(System.in);
 
 
+        int age = 10;
+        String nom = "david";
+
+
+        boolean a = (age >= 10) || (age >= 30) || (age >= 10);
+
+        if(age >= 10){
+            System.out.println("ceci est vrai que son age est >= 10 ");
+            if(nom.equals("david")){
+                System.out.println("il se nomme david");
+            }else{
+                System.out.println("Il ne se nomme pas David");
+            }
         }else{
-            System.out.println("Hey le gros, tu t'es trompé!");
-            System.exit(2);
-
+            System.out.println("ceci est faux!");
         }
+  
 
-        sc.close();
+        String categorie = "natation";
+
+        final String LIGNE1 = "Nom";
+
+
+        System.out.printf("%-10s:%n","Nom");
+        System.out.printf("%-10s:","Age");
+
+
     }
 }
 
