fdac05e626490890f5e1cfdc4740a5c20c8c2b62
David Brouillette - Enseignant Cégep de Trois-Rivières <63271086+dbrouillette@users.noreply.github.com>
2025-09-22 13:40:25 -0400

    Update Demot22Sept.java

--- Demo/Demot22Sept.java
+++ Demo/Demot22Sept.java
@@ -1,21 +1,30 @@
 package Demo;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Demot22Sept {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int nombre; 
-    boolean quitter = false; // variable de contrôle
+String[] choix = {"Roche", "Papier", "Ciseaux"};
+        Random alea = new Random();
 
-    while (!quitter) { // ou quitter == false
-    System.out.print("Entrez un nombre (0 pour quitter) : ");
-    nombre = scanner.nextInt();
-    if (nombre == 0) {
-        quitter = true; // la variable de contrôle change de valeur
-    }
-    System.out.println("Vous avez entré : " + nombre);
-    }
+        System.out.println("Choisissez : 0 = Roche, 1 = Papier, 2 = Ciseaux");
+        int joueur = 2; // votre choix, à modifier 
+        int ordinateur = alea.nextInt(3); // valeur aléatoire
+
+        System.out.println("Vous avez choisi : " + choix[joueur]);
+        System.out.println("L'ordinateur a choisi : " + choix[ordinateur]);
+
+        int resultat = (joueur - ordinateur + 3) % 3;
+
+        if (resultat == 0) {
+            System.out.println("Égalité !");
+        } else if (resultat == 1) {
+            System.out.println("Vous gagnez !");
+        } else {
+            System.out.println("L'ordinateur gagne !");
+        }
+
 
     }
 }
