57f8ed403f119226ad8376a59091fc36f7529ca5
David Brouillette - Enseignant Cégep de Trois-Rivières <63271086+dbrouillette@users.noreply.github.com>
2025-09-22 13:23:15 -0400

    Update Demot22Sept.java

--- Demo/Demot22Sept.java
+++ Demo/Demot22Sept.java
@@ -1,10 +1,21 @@
 package Demo;
 
+import java.util.Scanner;
+
 public class Demot22Sept {
     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int nombre; 
+    boolean quitter = false; // variable de contrôle
 
-
+    while (!quitter) { // ou quitter == false
+    System.out.print("Entrez un nombre (0 pour quitter) : ");
+    nombre = scanner.nextInt();
+    if (nombre == 0) {
+        quitter = true; // la variable de contrôle change de valeur
+    }
+    System.out.println("Vous avez entré : " + nombre);
+    }
 
     }
 }
-
