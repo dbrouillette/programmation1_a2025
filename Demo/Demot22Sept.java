2fb8f9fe135260b9d28b4e31d8a86631e689cbaf
David Brouillette - Enseignant Cégep de Trois-Rivières <63271086+dbrouillette@users.noreply.github.com>
2025-09-24 08:14:23 -0400

    Update Demot22Sept.java

--- Demo/Demot22Sept.java
+++ Demo/Demot22Sept.java
@@ -1,30 +1,26 @@
 package Demo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
 public class Demot22Sept {
     public static void main(String[] args) {
-String[] choix = {"Roche", "Papier", "Ciseaux"};
-        Random alea = new Random();
 
-        System.out.println("Choisissez : 0 = Roche, 1 = Papier, 2 = Ciseaux");
-        int joueur = 2; // votre choix, à modifier 
-        int ordinateur = alea.nextInt(3); // valeur aléatoire
+        int a = 0;
 
-        System.out.println("Vous avez choisi : " + choix[joueur]);
-        System.out.println("L'ordinateur a choisi : " + choix[ordinateur]);
-
-        int resultat = (joueur - ordinateur + 3) % 3;
-
-        if (resultat == 0) {
-            System.out.println("Égalité !");
-        } else if (resultat == 1) {
-            System.out.println("Vous gagnez !");
-        } else {
-            System.out.println("L'ordinateur gagne !");
-        }
-
+        do{
+            //System.out.println(a);
+            if(a%2 == 0){
+                System.out.printf("%d est un nom pair!%n", a);
+            }
+            a++;
+        }while(a<20);
+        
 
     }
 }
